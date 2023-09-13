afe91fb3f1947f0003fc6952b35ca263ea4167c4
Nasr Djeghmoum <larko236@outlook.com>
2023-09-13 13:05:52 -0400

    fix: remove submodules

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,19 @@
+import { Elysia } from 'elysia'
+import { yoga } from '@elysiajs/graphql-yoga'
+
+const app = new Elysia()
+    .use(
+        yoga({
+            typeDefs: /* GraphQL */`
+                type Query {
+                    hi: String
+                }
+            `,
+            resolvers: {
+                Query: {
+                    hi: () => 'Hello from Elysia'
+                }
+            }
+        })
+    )
+    .listen(3000)
