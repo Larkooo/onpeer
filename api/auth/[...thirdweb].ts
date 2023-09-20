3a0069491e5161074900f95806764dbeb518aca6
Nasr Djeghmoum <larko236@outlook.com>
2023-09-20 12:40:46 -0400

    feat(web): add formidable for file upload and livepeer sdk

--- api/auth/[...thirdweb].ts
+++ api/auth/[...thirdweb].ts
@@ -1,11 +1,11 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 
-const onpeerWallet = new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY!);
+const onpeerSigner = new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY!);
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
   domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN || "",
-  wallet: onpeerWallet,
+  wallet: onpeerSigner,
   // NOTE: All these callbacks are optional! You can delete this section and
   // the Auth flow will still work.
   callbacks: {
--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -2,13 +2,27 @@ import { ThirdwebSDK, getContract } from "@thirdweb-dev/sdk";
 import { getUser } from "./auth/[...thirdweb]";
 import { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
+import { studioProvider, createClient } from "@livepeer/react"
 
-const sdk = new ThirdwebSDK(process.env.CHAIN!, );
+const {provider: livepeer} = createClient({
+  provider: studioProvider({
+    apiKey: process.env.LIVEPEER_API_KEY!,
+  }),
+})
+const sdk = new ThirdwebSDK(process.env.CHAIN!);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
     return res.status(405).json({
       message: "Method not allowed.",
+    });
+  }
+
+  // Must be logged in
+  const user = await getUser(req);
+  if (!user) {
+    return res.status(401).json({
+      message: "Not authorized.",
     });
   }
 
@@ -20,27 +34,32 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       resolve({ fields, files });
     });
   });
+  
 
-
-  const user = await getUser(req);
-
-  if (!user) {
-    return res.status(401).json({
-      message: "Not authorized.",
+  const file = (data as any).files.file;
+  if (!file) {
+    return res.status(400).json({
+      message: "No file uploaded.",
     });
   }
 
+  const result = await livepeer.createAsset({
+    sources: [{
+      name: file.name,
+      file: file as any,
+    }],
+  });
+
+
   const onpeer = await sdk.getContract(process.env.ONPEER_CONTRACT_ADDRESS!);
-  onpeer.erc721.signature.generate({
+  const signature = await onpeer.erc721.signature.generate({
     to: user.address,
-    metadata: "",
+    metadata: result[0].id,
     quantity: 1,
     price: 0.1,
   });
 
-  return res.status(200).json({
-    message: `This is a secret for ${user.address}.`,
-  });
+  return res.status(200).json(signature);
 };
 
 export default handler;
