ee54b8eb2ef98a126e39b9570a51eacc48cfaa23
Nasr Djeghmoum <larko236@outlook.com>
2023-09-20 16:28:18 -0400

    feat(web): upload prompt

--- components/UploadFile.tsx
+++ components/UploadFile.tsx
@@ -10,10 +10,15 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useCallback } from "react";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { ButtonIcon, Cross2Icon } from "@radix-ui/react-icons";
+import { useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 interface UploadFileProps {
+  buttonText?: string;
+  title?: string;
+  description?: string;
   accept?: Record<string, string[]>;
   maxFiles?: number;
   maxSize?: number;
@@ -21,13 +26,21 @@ interface UploadFileProps {
 }
 
 export const UploadFile = ({
+  buttonText = "Upload",
+  title = "Upload",
+  description = "",
   onUpload,
   maxFiles = 1,
   accept,
   maxSize,
 }: UploadFileProps) => {
+  const [open, setOpen] = useState(false);
+
+  const [error, setError] = useState<string>();
+  const [files, setFiles] = useState<File[]>([]);
+
   const onDrop = useCallback((files: File[]) => {
-    onUpload(files);
+    setFiles(files);
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -36,29 +49,70 @@ export const UploadFile = ({
     accept,
     maxSize,
     multiple: maxFiles > 1,
+    onDropRejected(fileRejections, event) {
+      setError(fileRejections[0].errors[0].message);
+    },
   });
 
+  useEffect(() => {
+    if (!open) {
+      setFiles([]);
+      setError(undefined);
+    }
+  }, [open]);
+
   return (
-    <Dialog>
+    <Dialog onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button variant="outline">Upload</Button>
+        <Button variant="outline">{buttonText}</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Upload</DialogTitle>
-          <DialogDescription></DialogDescription>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+          <DialogDescription className="text-red-500">
+            {error}
+          </DialogDescription>
         </DialogHeader>
         <div
-          {...getRootProps()}
-          className="flex border-gray border-2 border-dashed h-20 w-full items-center justify-center text-sm"
+          {...(files.length === 0 ? getRootProps() : {})}
+          className={`flex border-gray border-2 ${
+            files.length === 0 && "border-dashed"
+          } h-20 w-full items-center justify-center text-sm flex-wrap`}
         >
-          <input {...getInputProps()} />
-          {isDragActive
+          {files.length === 0 && <input {...getInputProps()} />}
+          {files.length > 0
+            ? files.map((f, i) => (
+                <span className="w-full inline-flex items-center justify-center gap-4">
+                  <Cross2Icon
+                    onClick={() =>
+                      setFiles((files) => files.filter((_, m) => i !== m))
+                    }
+                  />
+                  {f.name}{" "}
+                </span>
+              ))
+            : isDragActive
             ? "Drop your files here"
             : "Drag 'n' drop your files here, or click to select files"}
         </div>
         <DialogFooter>
-          <Button type="submit">Upload</Button>
+          <div className="text-xs text-gray-400">
+            {Object.values(accept as any).join(", ")} supported.
+          </div>
+          <DialogClose>
+            <Button className="text-xs" variant="outline">
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button
+            onClick={() => files.length !== 0 && onUpload(files)}
+            className="text-xs"
+            type="submit"
+            disabled={files.length === 0}
+          >
+            Upload
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,10 @@ import {
   useMetamask,
 } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { UploadFile } from "components/UploadFile";
 import { Button } from "@/components/ui/button";
-
 
 const Home: NextPage = () => {
   const address = useAddress();
@@ -18,6 +17,17 @@ const Home: NextPage = () => {
   const { logout } = useLogout();
   const { user, isLoggedIn } = useUser();
   const [secret, setSecret] = useState();
+
+  const onUpload = useCallback(async (files: File[]) => {
+    console.log('kek')
+    const data = new FormData();
+    // data.append("file", files[0]);
+
+    const resSig = await fetch("/api/upload", {
+      method: "POST",
+      body: data,
+    });
+  }, []);
 
   return (
     <div>
@@ -28,10 +38,21 @@ const Home: NextPage = () => {
       ) : (
         <ConnectWallet theme="dark" />
       )}
-      <UploadFile />
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <UploadFile
+        title="Let's upload your video"
+        description="Once uploaded, your video will be stored on our peer to peer storage solution and you will then be prompted to mint your video NFT."
+        onUpload={onUpload}
+        accept={{
+          "video/mp4": [".mp4"],
+          "video/quicktime": [".mov"],
+          "video/x-msvideo": [".avi"],
+          "video/webm": [".webm"],
+          "video/x-ms-wmv": [".wmv"],
+          "video/x-matroska": [".mkv"],
+          "video/x-flv": [".flv"],
+        }}
+      />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <pre>Connected Wallet: {address}</pre>
       <pre>User: {JSON.stringify(user, undefined, 2) || "N/A"}</pre>
